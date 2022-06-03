9d7f534e1f0910bff05dbdacd0447ba1bd1b4159
gokul <pmgokul7@gmail.com>
2022-06-03 22:12:43 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,46 @@ import Home from './components/home/home';
 import Navbar from './components/navbar/navbar';
 import Skills from './components/skills/skills';
 import s  from "./images/facebook.png"
+import {useRef} from "react"
 
 function App() {
-  <script>
-    AOS.init();
-  </script>
+
+  const aboutref=useRef(null)
+  const skillsref=useRef(null)
+  const contactref=useRef(null)
+  const homeref=useRef(null)
+ 
   return (
     <div className="App">
-    <Navbar/>
-    <Home/>
-    <About/>
-    <Skills/>
-    <Contact/>
+         <div className="navbar_container">
+               <div className="left_navbar">
+                  <h1><span>G</span></h1>
+               </div>
+               <div className="right_navbar">
+                    
+                       <a onClick={()=>homeref.current.scrollIntoView()}>Home</a>            
+                      <a onClick={()=>aboutref.current.scrollIntoView()}>About</a>
+                      <a onClick={()=>skillsref.current.scrollIntoView()}>Skills</a>
+                      <a onClick={()=>contactref.current.scrollIntoView()}>Contact</a>
+
+               </div>
+           </div>
+        <div className="home" ref={homeref}>
+              <Home/>
+        </div>
+        <div className="about" ref={aboutref}>
+        <About/>
+        </div>
+
+    <div className="skil" ref={skillsref}>
+    <Skills />
+    </div>
+    
+    <div className="contact" ref={contactref}>
+       <Contact/>
+    </div>
+    
+   
     
       
       
